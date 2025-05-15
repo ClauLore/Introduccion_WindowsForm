c1a5b92da98e10efb3724b3a35e43a83afe7242d
Claudia <claudialo.garciar@gmail.com>
2025-05-14 20:37:28 -0500

    Agregar archivos de proyecto.

---
+++ Introduccion_WindowsForm/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Introduccion_WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdd = new Button();
+            label1 = new Label();
+            txtUsuario = new TextBox();
+            txtPassword = new TextBox();
+            label2 = new Label();
+            progressBar1 = new ProgressBar();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(165, 114);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(107, 39);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Guardar";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(92, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(213, 22);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(100, 23);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(213, 62);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(100, 23);
+            txtPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(92, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Contraseña";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(114, 192);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(199, 23);
+            progressBar1.TabIndex = 9;
+            progressBar1.Value = 50;
+            progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(443, 266);
+            Controls.Add(progressBar1);
+            Controls.Add(txtPassword);
+            Controls.Add(label2);
+            Controls.Add(txtUsuario);
+            Controls.Add(label1);
+            Controls.Add(btnAdd);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAdd;
+        private Label label1;
+        private TextBox txtUsuario;
+        private TextBox txtPassword;
+        private Label label2;
+        private ProgressBar progressBar1;
+    }
+}
---
+++ Introduccion_WindowsForm/Form1.cs
@@ -0,0 +1,43 @@
+namespace Introduccion_WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string ls_usuario = txtUsuario.Text;
+            string ls_password = txtPassword.Text;
+
+            if (ls_usuario.Trim().Length == 0)
+            {
+                MessageBox.Show("Ingrese un usuario válido");
+                txtUsuario.Focus();
+                return;
+            }
+
+            if (ls_password.Trim().Length == 0)
+            {
+                MessageBox.Show("Ingrese un password válido");
+                txtPassword.Focus();
+                return;
+            }
+
+            if (ls_usuario=="cgarcia" && ls_password == "cl@u2025**")
+            {
+                Form2 form2 = new Form2();
+                
+                progressBar1.Visible = true;
+                progressBar1.Value = 100;
+                form2.ShowDialog();
+            }
+
+
+           
+        }
+
+       
+    }
+}
